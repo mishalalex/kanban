390b06de9d829e5ed10ad0c31ea592a4a7d51d22
Mishal <mishalalexander20@gmail.com"n>
2025-03-15 17:37:33 +0530

    adding add new board functionalty
    Signed-off-by: Mishal <mishalalexander20@gmail.com"n>

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-const addTask = document.querySelector("#add-task-btn");
-const todoBoard = document.querySelector("#todo-board");
+const boardContainer = document.querySelector(".container");
+const firstBoard = document.querySelector(".container").firstElementChild;
 const board = document.querySelectorAll(".board");
+const addTask = document.querySelector("#add-task-btn");
+const addBoard = document.querySelector("#add-board-btn");
+const task = document.querySelector(".item");
+
 
 function dragOver(element) {
     element.addEventListener("dragover", () => {
         const elementComingAbove = document.querySelector(".flying");
         elementComingAbove.addEventListener("dragend", () => {
-            element.appendChild(elementComingAbove)
+            element.appendChild(elementComingAbove);
         })
     })
 }
+
+addBoard.addEventListener("click", () => {
+    const input = prompt("Enter the board name");
+    if (!input) return;
+    const newBoard = document.createElement("div");
+    newBoard.classList.add("board")
+    const hTwoElement = document.createElement("h2");
+    hTwoElement.classList.add("board-title");
+    hTwoElement.textContent = input;
+    newBoard.appendChild(hTwoElement);
+    boardContainer.insertBefore(newBoard, addBoard)
+})
+
 
 addTask.addEventListener("click", () => {
     const input = prompt("Enter a task");
@@ -24,7 +41,9 @@ addTask.addEventListener("click", () => {
     newTask.addEventListener("dragend", () => {
         newTask.classList.remove("flying");
     })
-    todoBoard.appendChild(newTask);
+    if (firstBoard) {
+        firstBoard.appendChild(newTask);
+    }
 })
 
 board.forEach(dragOver)
